3a35fc478ca4163b59c381c363edd0d9d291b63d
Francois A <francoisancel@gmail.com>
2019-03-25 16:40:18 +0100

    Adding opencv dependancy to cmake

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
-
+#include <opencv2/opencv.hpp>
+#include <stdio.h>
 
 int main(int argc, char** argv)
 {
-	return 0;
+	if (argc != 2)
+    {
+        printf("usage: DisplayImage.out <Image_Path>\n");
+        return -1;
+    }
+    cv::Mat image;
+    image = cv::imread(argv[1], 1);
+    if (!image.data)
+    {
+        printf("No image data \n");
+        return -1;
+    }
+    cv::namedWindow("Display Image", cv::WINDOW_AUTOSIZE);
+    cv::imshow("Display Image", image);
+    cv::waitKey(0);
+    return 0;
 }
