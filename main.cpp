2129ad881efa6e66dc279a408fd0b60a74f57d3b
Francois A <francoisancel@gmail.com>
2019-03-27 16:42:46 +0100

    keypoint approach

--- main.cpp
+++ main.cpp
@@ -4,19 +4,59 @@
 constexpr const char* model = R".(C:\Users\fancel\Documents\ArcheryCounter\Images\model\A53C085.jpg).";
 constexpr const char* test = R".(C:\Users\fancel\Documents\ArcheryCounter\Images\20190325_193217.jpg).";
 
-std::pair<std::vector<cv::KeyPoint>, cv::Mat> compute_keypoint(const cv::Mat& image)
+template <typename PointT> auto euclideanDistance(const PointT& lval, const PointT& rval)
 {
-    auto orb = cv::ORB::create();
-    std::vector<cv::KeyPoint> result;
-    cv::Mat descriptor;
-    orb->detectAndCompute(image, cv::noArray(), result, descriptor);
-    return {result, descriptor};
+    auto diff = lval - rval;
+    return cv::sqrt((diff.x * diff.x) + (diff.y * diff.y));
 }
+
+std::vector<cv::DMatch> filter_match(const std::vector<cv::KeyPoint>& keypoint_model,
+    const std::vector<cv::KeyPoint>& keypoint_test, const std::vector<std::vector<cv::DMatch>>& matches,
+    double lowe_ratio)
+{
+    std::vector<cv::DMatch> result;
+    for(const auto& vals : matches)
+    {
+        if(vals[0].distance < lowe_ratio * vals[1].distance)
+        {
+            result.push_back(vals[0]);
+        }
+    }
+    return result;
+}
+
+void keypoint_approach(const cv::Mat& image_model, const cv::Mat& image_test) {
+    auto orb = cv::ORB::create();
+    std::vector<cv::KeyPoint> keypoint_model, keypoint_test;
+    cv::Mat descriptor_model, descriptor_test;
+
+    orb->detectAndCompute(image_model, cv::noArray(), keypoint_model, descriptor_model, false);
+    orb->detectAndCompute(image_test, cv::noArray(), keypoint_test, descriptor_test, false);
+
+    auto matcher = cv::DescriptorMatcher::create(cv::DescriptorMatcher::BRUTEFORCE_HAMMING);
+    std::vector<std::vector<cv::DMatch>> knn_matches;
+    matcher->knnMatch(descriptor_model, descriptor_test, knn_matches, 2);
+
+    const float ratio_thresh = 0.75f;
+    std::vector<cv::DMatch> filtered_matches = filter_match(keypoint_model, keypoint_test, knn_matches, ratio_thresh);
+
+    cv::Mat img_matches;
+
+    // cv::drawKeypoints(image_model, keypoint_model, detected, cv::Scalar(0, 255, 0));
+    drawMatches(image_model, keypoint_model, image_test, keypoint_test, filtered_matches, img_matches);
+
+    cv::namedWindow("Display Image", cv::WINDOW_NORMAL);
+    cv::imshow("Display Image", img_matches);
+    cv::waitKey(0);
+    cv::destroyAllWindows();
+}
+
+
 
 int main(int argc, char** argv)
 {
-    cv::Mat image_model = cv::imread(model, cv::IMREAD_GRAYSCALE);
-    cv::Mat image_test = cv::imread(test, cv::IMREAD_GRAYSCALE);
+    cv::Mat image_model = cv::imread(model, cv::IMREAD_COLOR);
+    cv::Mat image_test = cv::imread(test, cv::IMREAD_COLOR);
 
     if(!image_model.data || !image_test.data)
     {
@@ -24,31 +64,6 @@ int main(int argc, char** argv)
         return -1;
     }
 
-    auto [keypoint_model, descriptor_model] = compute_keypoint(image_model);
-    auto [keypoint_test, descriptor_test] = compute_keypoint(image_test);
-
-    //auto matcher = cv::DescriptorMatcher::create(cv::DescriptorMatcher::FLANNBASED);
-    //std::vector<std::vector<cv::DMatch>> knn_matches;
-    //matcher->knnMatch(descriptor_model, descriptor_test, knn_matches, 2);
-
-    //const float ratio_thresh = 0.75f;
-    //std::vector<cv::DMatch> good_matches;
-    //for(std::size_t i = 0; i < knn_matches.size(); i++)
-    //{
-    //    if(knn_matches[i][0].distance < (ratio_thresh * knn_matches[i][1].distance))
-    //    {
-    //        good_matches.push_back(knn_matches[i][0]);
-    //    }
-    //}
-
-    cv::Mat img_matches;
-
-    cv::Mat detected;
-    cv::drawKeypoints(image_model, keypoint_model, detected, cv::Scalar(0, 255, 0));
-
-    cv::namedWindow("Display Image", cv::WINDOW_NORMAL);
-    cv::imshow("Display Image", detected);
-    cv::waitKey(0);
-    cv::destroyAllWindows();
-    return 0;
+	//keypoint_approach(image_model, image_test);
+	return 0;
 }
